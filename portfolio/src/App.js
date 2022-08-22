d8e63e7d9bf00f70f4a35bd6d6e88d8133e22567
HamaydaGabsi <hamayda.gabsi@insat.ucar.tn>
2022-08-22 13:18:08 +0100

    Created Skelleton + Typography theming

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+import Navbar from "./Navbar";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const typographyTheme = {
+    fontSizes: ["4.369rem", "1.777rem", "1.333rem", "1rem", "0.75rem", "10px"],
+    bodyFontFamily: "Source Code Pro, Input, monospace",
+    headingFontFamily:
+      "Oswald, SF Display, Helvetica Neue, Circular, sans-serif",
+  };
+  const ThemeWrapper = styled.div`
+    & h1 {
+      font-family: 'Oswald';
+      font-weight: 500;
+      font-size: 118.7px;
+      line-height: 3rem;
+    }
+    & a {
+      font-family: 'RobotoSlab';
+      font-size: 16px;
+    }
+    & p , button{
+      font-family: 'RobotoSlab';
+      font-size: 21px;
+    }
+  `;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={{ typography: typographyTheme }}>
+      <ThemeWrapper>
+        <div>
+          <Navbar />
+        </div>
+      </ThemeWrapper>
+    </ThemeProvider>
   );
 }
 
